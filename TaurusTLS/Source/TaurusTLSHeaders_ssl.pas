5507c981d82c705cf259b1789b1403abecde540d
J. Peter Mugaas <jpmugaas@suddenlink.net>
2025-07-06 02:32:23 -0400

    Rename   TaurusTLSHeaders_ossl_typ to   TaurusTLSHeaders_types{ for consistancy with OpenSSL 3.x headers.

--- TaurusTLS/Source/TaurusTLSHeaders_ssl.pas
+++ TaurusTLS/Source/TaurusTLSHeaders_ssl.pas
@@ -45,7 +45,7 @@ uses
   {$IFDEF OPENSSL_STATIC_LINK_MODEL}
   TaurusTLSConsts,
   {$ENDIF}
-  TaurusTLSHeaders_ossl_typ,
+  TaurusTLSHeaders_types,
   TaurusTLSHeaders_async,
   TaurusTLSHeaders_bio,
   TaurusTLSHeaders_crypto,
